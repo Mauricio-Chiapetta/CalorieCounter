6a21608312cfd2b5b077c0b082c5708a0c2ccbce
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-03-19 18:26:32 -0300

    commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget')
-const entryDropdown = document.getElementById('entry-dropdown')
-const addEntryButton = document.getElementById('add-entry')
-const clearButton = document.getElementById('clear')
-const output = document.getElementById('output')
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
 
-let isError = false
+let isError = false;
 
 // funçao antiga(usa muito mais memoria , queremos otimizar !)
 // function cleanInputString(str){
@@ -17,22 +17,38 @@ let isError = false
 //         if(!["+","-"," "].includes(strArray[i])){
 //             cleanStrArray.push(strArray[i])
 //         }
-                
+
 //     }
 // }
 
 // atualizada com regex (menos memória e mais otimizado)
-function cleanInputString (str){
-    const regex  = /[+-\s]/g
-    return str.replace(regex,'')
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
 }
 
-function isIsvalidInput(str){
-    const regex = /\d+e\d+/i
-    return str.match(regex)
+function isIsvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+// nao entendi o que essa função faz ainda
+function addEntry() {
+    //  const  targetId = "#"+entryDropdown.value
+    const targetInputContainer = document.querySelector(
+        `#${entryDropdown.value} .input-container`
+    ); // #lunch input-container(exemplo de saída)
+    const entryNumber =
+        targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `<label  for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+   <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+   <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`;
+
+   targetInputContainer.insertAdjacentHTML("beforeend",HTMLString)
 }
 
-function addEntry(){
-   const  targetId = "#"+entryDropdown.value
-   const targetInputContainer = document.querySelector(`${targetId} .input-container`) // #lunch input-container
+addEntryButton.addEventListener('click',addEntry)
+
+function getCaloriesFromInputs(list){
+let calories = 0
 }
